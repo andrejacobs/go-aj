8a7b38952d0311d77c6d486699163d68ab3b5533
Andre Jacobs <andrejacobs@users.noreply.github.com>
2025-09-25 21:38:43 +0100

    Added more safe casts

---
+++ ajmath/safe.go
@@ -0,0 +1,255 @@
+// Copyright (c) 2025 Andre Jacobs
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+package ajmath
+
+import (
+	"errors"
+	"math"
+	"math/bits"
+)
+
+var (
+	// An integer overflow occurred.
+	ErrIntegerOverflow = errors.New("integer overflow occurred")
+
+	// An integer underflow occurred.
+	ErrIntegerUnderflow = errors.New("integer underflow occurred")
+)
+
+// Add two unsigned 32bit integers.
+// Returns [ErrIntegerOverflow] if an overflow occurred.
+func Add32(x, y uint32) (uint32, error) {
+	new, overflowed := bits.Add32(x, y, 0)
+	if overflowed > 0 {
+		return 0, ErrIntegerOverflow
+	}
+	return new, nil
+}
+
+// Add two unsigned 64bit integers.
+// Returns [ErrIntegerOverflow] if an overflow occurred.
+func Add64(x, y uint64) (uint64, error) {
+	new, overflowed := bits.Add64(x, y, 0)
+	if overflowed > 0 {
+		return 0, ErrIntegerOverflow
+	}
+	return new, nil
+}
+
+// Subtract two unsigned 32bit integers.
+// Returns [ErrIntegerUnderflow] if an underflow occurred.
+func Sub32(x, y uint32) (uint32, error) {
+	new, underflowed := bits.Sub32(x, y, 0)
+	if underflowed > 0 {
+		return 0, ErrIntegerUnderflow
+	}
+	return new, nil
+}
+
+// Subtract two unsigned 64bit integers.
+// Returns [ErrIntegerUnderflow] if an underflow occurred.
+func Sub64(x, y uint64) (uint64, error) {
+	new, underflowed := bits.Sub64(x, y, 0)
+	if underflowed > 0 {
+		return 0, ErrIntegerUnderflow
+	}
+	return new, nil
+}
+
+//-----------------------------------------------------------------------------
+// Safe casting
+
+// Cast from a signed 8bit integer to an unsigned 8bit integer.
+// Return [ErrIntegerUnderflow] if x contains a negative number.
+func Int8ToUint8(x int8) (uint8, error) {
+	if x < 0 {
+		return 0, ErrIntegerUnderflow
+	}
+	return uint8(x), nil
+}
+
+// Cast from an unsigned 8bit integer to a signed 8bit integer.
+// Return [ErrIntegerOverflow] if x is too big.
+func Uint8ToInt8(x uint8) (int8, error) {
+	if x > math.MaxInt8 {
+		return 0, ErrIntegerOverflow
+	}
+	return int8(x), nil
+}
+
+// Cast from a signed 16bit integer to an unsigned 16bit integer.
+// Return [ErrIntegerUnderflow] if x contains a negative number.
+func Int16ToUint16(x int16) (uint16, error) {
+	if x < 0 {
+		return 0, ErrIntegerUnderflow
+	}
+	return uint16(x), nil
+}
+
+// Cast from an unsigned 16bit integer to a signed 16bit integer.
+// Return [ErrIntegerOverflow] if x is too big.
+func Uint16ToInt16(x uint16) (int16, error) {
+	if x > math.MaxInt16 {
+		return 0, ErrIntegerOverflow
+	}
+	return int16(x), nil
+}
+
+// Cast from a signed 32bit integer to an unsigned 32bit integer.
+// Return [ErrIntegerUnderflow] if x contains a negative number.
+func Int32ToUint32(x int32) (uint32, error) {
+	if x < 0 {
+		return 0, ErrIntegerUnderflow
+	}
+	return uint32(x), nil
+}
+
+// Cast from an unsigned 32bit integer to a signed 32bit integer.
+// Return [ErrIntegerOverflow] if x is too big.
+func Uint32ToInt32(x uint32) (int32, error) {
+	if x > math.MaxInt32 {
+		return 0, ErrIntegerOverflow
+	}
+	return int32(x), nil
+}
+
+// Cast from a signed 64bit integer to an unsigned 64bit integer.
+// Return [ErrIntegerUnderflow] if x contains a negative number.
+func Int64ToUint64(x int64) (uint64, error) {
+	if x < 0 {
+		return 0, ErrIntegerUnderflow
+	}
+	return uint64(x), nil
+}
+
+// Cast from an unsigned 64bit integer to a signed 64bit integer.
+// Return [ErrIntegerOverflow] if x is too big.
+func Uint64ToInt64(x uint64) (int64, error) {
+	if x > math.MaxInt64 {
+		return 0, ErrIntegerOverflow
+	}
+	return int64(x), nil
+}
+
+//-----------------------------------------------------------------------------
+// Downcasting
+
+// Downcast an unsigned 64bit integer to an unsigned 32bit integer.
+// Returns [ErrIntegerOverflow] if an overflow occurred.
+func Uint64ToUint32(x uint64) (uint32, error) {
+	if x > math.MaxUint32 {
+		return 0, ErrIntegerOverflow
+	}
+	return uint32(x), nil
+}
+
+// Downcast a signed 64bit integer to a signed 32bit integer.
+// Returns [ErrIntegerOverflow] if an overflow occurred.
+func Int64ToInt32(x int64) (int32, error) {
+	if x > math.MaxInt32 {
+		return 0, ErrIntegerOverflow
+	}
+	return int32(x), nil
+}
+
+// Downcast a signed 64bit integer to an unsigned 32bit integer.
+// Returns [ErrIntegerUnderflow] if x is negative.
+// Returns [ErrIntegerOverflow] if x is too big.
+func Int64ToUint32(x int64) (uint32, error) {
+	if x < 0 {
+		return 0, ErrIntegerUnderflow
+	} else if x > math.MaxUint32 {
+		return 0, ErrIntegerOverflow
+	}
+	return uint32(x), nil
+}
+
+// Downcast an unsigned 64bit integer to a signed 32bit integer.
+// Returns [ErrIntegerOverflow] if x is too big.
+func Uint64ToInt32(x uint64) (int32, error) {
+	if x > math.MaxInt32 {
+		return 0, ErrIntegerOverflow
+	}
+	return int32(x), nil
+}
+
+// Cast from platform dependant signed integer to a signed 8bit integer.
+// Return [ErrIntegerUnderflow] if x is too small.
+// Return [ErrIntegerOverflow] if x is too big.
+func IntToInt8(x int) (int8, error) {
+	if x < math.MinInt8 {
+		return 0, ErrIntegerUnderflow
+	} else if x > math.MaxInt8 {
+		return 0, ErrIntegerOverflow
+	}
+	return int8(x), nil
+}
+
+// Cast from platform dependant signed integer to a signed 16bit integer.
+// Return [ErrIntegerUnderflow] if x is too small.
+// Return [ErrIntegerOverflow] if x is too big.
+func IntToInt16(x int) (int16, error) {
+	if x < math.MinInt16 {
+		return 0, ErrIntegerUnderflow
+	} else if x > math.MaxInt16 {
+		return 0, ErrIntegerOverflow
+	}
+	return int16(x), nil
+}
+
+// Cast from platform dependant signed integer to a signed 32bit integer.
+// Return [ErrIntegerUnderflow] if x is too small.
+// Return [ErrIntegerOverflow] if x is too big.
+func IntToInt32(x int) (int32, error) {
+	if x < math.MinInt32 {
+		return 0, ErrIntegerUnderflow
+	} else if x > math.MaxInt32 {
+		return 0, ErrIntegerOverflow
+	}
+	return int32(x), nil
+}
+
+// Cast from platform dependant unsigned integer to an unsigned 8bit integer.
+// Return [ErrIntegerOverflow] if x is too big.
+func UintToUint8(x uint) (uint8, error) {
+	if x > math.MaxUint8 {
+		return 0, ErrIntegerOverflow
+	}
+	return uint8(x), nil
+}
+
+// Cast from platform dependant unsigned integer to an unsigned 16bit integer.
+// Return [ErrIntegerOverflow] if x is too big.
+func UintToUint16(x uint) (uint16, error) {
+	if x > math.MaxUint16 {
+		return 0, ErrIntegerOverflow
+	}
+	return uint16(x), nil
+}
+
+// Cast from platform dependant unsigned integer to an unsigned 32bit integer.
+// Return [ErrIntegerOverflow] if x is too big.
+func UintToUint32(x uint) (uint32, error) {
+	if x > math.MaxUint32 {
+		return 0, ErrIntegerOverflow
+	}
+	return uint32(x), nil
+}
---
+++ ajmath/safe_test.go
@@ -0,0 +1,387 @@
+// Copyright (c) 2025 Andre Jacobs
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+package ajmath_test
+
+import (
+	"math"
+	"testing"
+
+	"github.com/andrejacobs/go-aj/ajmath"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestAdd32(t *testing.T) {
+	v, err := ajmath.Add32(42, 42)
+	assert.NoError(t, err)
+	assert.Equal(t, uint32(84), v)
+
+	v, err = ajmath.Add32(42, uint32(math.MaxUint32))
+	assert.ErrorIs(t, err, ajmath.ErrIntegerOverflow)
+	assert.Equal(t, uint32(0), v)
+}
+
+func TestAdd64(t *testing.T) {
+	v, err := ajmath.Add64(42, 42)
+	assert.NoError(t, err)
+	assert.Equal(t, uint64(84), v)
+
+	v, err = ajmath.Add64(42, uint64(math.MaxUint64))
+	assert.ErrorIs(t, err, ajmath.ErrIntegerOverflow)
+	assert.Equal(t, uint64(0), v)
+}
+
+func TestSub32(t *testing.T) {
+	v, err := ajmath.Sub32(42, 42)
+	assert.NoError(t, err)
+	assert.Equal(t, uint32(0), v)
+
+	v, err = ajmath.Sub32(42, 45)
+	assert.ErrorIs(t, err, ajmath.ErrIntegerUnderflow)
+	assert.Equal(t, uint32(0), v)
+}
+
+func TestSub64(t *testing.T) {
+	v, err := ajmath.Sub64(42, 42)
+	assert.NoError(t, err)
+	assert.Equal(t, uint64(0), v)
+
+	v, err = ajmath.Sub64(42, 45)
+	assert.ErrorIs(t, err, ajmath.ErrIntegerUnderflow)
+	assert.Equal(t, uint64(0), v)
+}
+
+//-----------------------------------------------------------------------------
+// Casting
+
+func TestInt8ToUint8(t *testing.T) {
+	v, err := ajmath.Int8ToUint8(0)
+	assert.NoError(t, err)
+	assert.Equal(t, uint8(0), v)
+
+	v, err = ajmath.Int8ToUint8(math.MaxInt8)
+	assert.NoError(t, err)
+	assert.Equal(t, uint8(math.MaxInt8), v)
+
+	v, err = ajmath.Int8ToUint8(-42)
+	assert.ErrorIs(t, err, ajmath.ErrIntegerUnderflow)
+	assert.Equal(t, uint8(0), v)
+}
+
+func TestUint8ToInt8(t *testing.T) {
+	v, err := ajmath.Uint8ToInt8(0)
+	assert.NoError(t, err)
+	assert.Equal(t, int8(0), v)
+
+	v, err = ajmath.Uint8ToInt8(math.MaxInt8)
+	assert.NoError(t, err)
+	assert.Equal(t, int8(math.MaxInt8), v)
+
+	v, err = ajmath.Uint8ToInt8(math.MaxInt8 + 1)
+	assert.ErrorIs(t, err, ajmath.ErrIntegerOverflow)
+	assert.Equal(t, int8(0), v)
+}
+
+func TestInt16ToUint16(t *testing.T) {
+	v, err := ajmath.Int16ToUint16(0)
+	assert.NoError(t, err)
+	assert.Equal(t, uint16(0), v)
+
+	v, err = ajmath.Int16ToUint16(math.MaxInt16)
+	assert.NoError(t, err)
+	assert.Equal(t, uint16(math.MaxInt16), v)
+
+	v, err = ajmath.Int16ToUint16(-42)
+	assert.ErrorIs(t, err, ajmath.ErrIntegerUnderflow)
+	assert.Equal(t, uint16(0), v)
+}
+
+func TestUint16ToInt16(t *testing.T) {
+	v, err := ajmath.Uint16ToInt16(0)
+	assert.NoError(t, err)
+	assert.Equal(t, int16(0), v)
+
+	v, err = ajmath.Uint16ToInt16(math.MaxInt16)
+	assert.NoError(t, err)
+	assert.Equal(t, int16(math.MaxInt16), v)
+
+	v, err = ajmath.Uint16ToInt16(math.MaxInt16 + 1)
+	assert.ErrorIs(t, err, ajmath.ErrIntegerOverflow)
+	assert.Equal(t, int16(0), v)
+}
+
+func TestInt32ToUint32(t *testing.T) {
+	v, err := ajmath.Int32ToUint32(0)
+	assert.NoError(t, err)
+	assert.Equal(t, uint32(0), v)
+
+	v, err = ajmath.Int32ToUint32(math.MaxInt32)
+	assert.NoError(t, err)
+	assert.Equal(t, uint32(math.MaxInt32), v)
+
+	v, err = ajmath.Int32ToUint32(-42)
+	assert.ErrorIs(t, err, ajmath.ErrIntegerUnderflow)
+	assert.Equal(t, uint32(0), v)
+}
+
+func TestUint32ToInt32(t *testing.T) {
+	v, err := ajmath.Uint32ToInt32(0)
+	assert.NoError(t, err)
+	assert.Equal(t, int32(0), v)
+
+	v, err = ajmath.Uint32ToInt32(math.MaxInt32)
+	assert.NoError(t, err)
+	assert.Equal(t, int32(math.MaxInt32), v)
+
+	v, err = ajmath.Uint32ToInt32(math.MaxInt32 + 1)
+	assert.ErrorIs(t, err, ajmath.ErrIntegerOverflow)
+	assert.Equal(t, int32(0), v)
+}
+
+func TestInt64ToUint64(t *testing.T) {
+	v, err := ajmath.Int64ToUint64(0)
+	assert.NoError(t, err)
+	assert.Equal(t, uint64(0), v)
+
+	v, err = ajmath.Int64ToUint64(math.MaxInt64)
+	assert.NoError(t, err)
+	assert.Equal(t, uint64(math.MaxInt64), v)
+
+	v, err = ajmath.Int64ToUint64(-42)
+	assert.ErrorIs(t, err, ajmath.ErrIntegerUnderflow)
+	assert.Equal(t, uint64(0), v)
+}
+
+func TestUint64ToInt64(t *testing.T) {
+	v, err := ajmath.Uint64ToInt64(0)
+	assert.NoError(t, err)
+	assert.Equal(t, int64(0), v)
+
+	v, err = ajmath.Uint64ToInt64(math.MaxInt64)
+	assert.NoError(t, err)
+	assert.Equal(t, int64(math.MaxInt64), v)
+
+	v, err = ajmath.Uint64ToInt64(math.MaxInt64 + 1)
+	assert.ErrorIs(t, err, ajmath.ErrIntegerOverflow)
+	assert.Equal(t, int64(0), v)
+}
+
+//-----------------------------------------------------------------------------
+// Downcasting
+
+func TestUint64ToUint32(t *testing.T) {
+	v, err := ajmath.Uint64ToUint32(0)
+	assert.NoError(t, err)
+	assert.Equal(t, uint32(0), v)
+
+	v, err = ajmath.Uint64ToUint32(4242)
+	assert.NoError(t, err)
+	assert.Equal(t, uint32(4242), v)
+
+	v, err = ajmath.Uint64ToUint32(math.MaxUint32)
+	assert.NoError(t, err)
+	assert.Equal(t, uint32(math.MaxUint32), v)
+
+	v, err = ajmath.Uint64ToUint32(math.MaxUint32 + 1)
+	assert.ErrorIs(t, err, ajmath.ErrIntegerOverflow)
+	assert.Equal(t, uint32(0), v)
+}
+
+func TestInt64ToInt32(t *testing.T) {
+	v, err := ajmath.Int64ToInt32(0)
+	assert.NoError(t, err)
+	assert.Equal(t, int32(0), v)
+
+	v, err = ajmath.Int64ToInt32(4242)
+	assert.NoError(t, err)
+	assert.Equal(t, int32(4242), v)
+
+	v, err = ajmath.Int64ToInt32(math.MaxInt32)
+	assert.NoError(t, err)
+	assert.Equal(t, int32(math.MaxInt32), v)
+
+	v, err = ajmath.Int64ToInt32(math.MaxInt32 + 1)
+	assert.ErrorIs(t, err, ajmath.ErrIntegerOverflow)
+	assert.Equal(t, int32(0), v)
+}
+
+func TestInt64ToUint32(t *testing.T) {
+	v, err := ajmath.Int64ToUint32(0)
+	assert.NoError(t, err)
+	assert.Equal(t, uint32(0), v)
+
+	v, err = ajmath.Int64ToUint32(4242)
+	assert.NoError(t, err)
+	assert.Equal(t, uint32(4242), v)
+
+	v, err = ajmath.Int64ToUint32(-42)
+	assert.ErrorIs(t, err, ajmath.ErrIntegerUnderflow)
+	assert.Equal(t, uint32(0), v)
+
+	v, err = ajmath.Int64ToUint32(math.MaxUint32 + 1)
+	assert.ErrorIs(t, err, ajmath.ErrIntegerOverflow)
+	assert.Equal(t, uint32(0), v)
+}
+
+func TestUint64ToInt32(t *testing.T) {
+	v, err := ajmath.Uint64ToInt32(0)
+	assert.NoError(t, err)
+	assert.Equal(t, int32(0), v)
+
+	v, err = ajmath.Uint64ToInt32(4242)
+	assert.NoError(t, err)
+	assert.Equal(t, int32(4242), v)
+
+	v, err = ajmath.Uint64ToInt32(math.MaxInt32 + 1)
+	assert.ErrorIs(t, err, ajmath.ErrIntegerOverflow)
+	assert.Equal(t, int32(0), v)
+}
+
+func TestIntToInt8(t *testing.T) {
+	v, err := ajmath.IntToInt8(0)
+	assert.NoError(t, err)
+	assert.Equal(t, int8(0), v)
+
+	v, err = ajmath.IntToInt8(42)
+	assert.NoError(t, err)
+	assert.Equal(t, int8(42), v)
+
+	v, err = ajmath.IntToInt8(math.MinInt8)
+	assert.NoError(t, err)
+	assert.Equal(t, int8(math.MinInt8), v)
+
+	v, err = ajmath.IntToInt8(math.MaxInt8)
+	assert.NoError(t, err)
+	assert.Equal(t, int8(math.MaxInt8), v)
+
+	v, err = ajmath.IntToInt8(math.MinInt8 - 1)
+	assert.ErrorIs(t, err, ajmath.ErrIntegerUnderflow)
+	assert.Equal(t, int8(0), v)
+
+	v, err = ajmath.IntToInt8(math.MaxInt8 + 1)
+	assert.ErrorIs(t, err, ajmath.ErrIntegerOverflow)
+	assert.Equal(t, int8(0), v)
+}
+
+func TestIntToInt16(t *testing.T) {
+	v, err := ajmath.IntToInt16(0)
+	assert.NoError(t, err)
+	assert.Equal(t, int16(0), v)
+
+	v, err = ajmath.IntToInt16(42)
+	assert.NoError(t, err)
+	assert.Equal(t, int16(42), v)
+
+	v, err = ajmath.IntToInt16(math.MinInt16)
+	assert.NoError(t, err)
+	assert.Equal(t, int16(math.MinInt16), v)
+
+	v, err = ajmath.IntToInt16(math.MaxInt16)
+	assert.NoError(t, err)
+	assert.Equal(t, int16(math.MaxInt16), v)
+
+	v, err = ajmath.IntToInt16(math.MinInt16 - 1)
+	assert.ErrorIs(t, err, ajmath.ErrIntegerUnderflow)
+	assert.Equal(t, int16(0), v)
+
+	v, err = ajmath.IntToInt16(math.MaxInt16 + 1)
+	assert.ErrorIs(t, err, ajmath.ErrIntegerOverflow)
+	assert.Equal(t, int16(0), v)
+}
+
+func TestIntToInt32(t *testing.T) {
+	v, err := ajmath.IntToInt32(0)
+	assert.NoError(t, err)
+	assert.Equal(t, int32(0), v)
+
+	v, err = ajmath.IntToInt32(42)
+	assert.NoError(t, err)
+	assert.Equal(t, int32(42), v)
+
+	v, err = ajmath.IntToInt32(math.MinInt32)
+	assert.NoError(t, err)
+	assert.Equal(t, int32(math.MinInt32), v)
+
+	v, err = ajmath.IntToInt32(math.MaxInt32)
+	assert.NoError(t, err)
+	assert.Equal(t, int32(math.MaxInt32), v)
+
+	v, err = ajmath.IntToInt32(math.MinInt32 - 1)
+	assert.ErrorIs(t, err, ajmath.ErrIntegerUnderflow)
+	assert.Equal(t, int32(0), v)
+
+	v, err = ajmath.IntToInt32(math.MaxInt32 + 1)
+	assert.ErrorIs(t, err, ajmath.ErrIntegerOverflow)
+	assert.Equal(t, int32(0), v)
+}
+
+func TestUintToUint8(t *testing.T) {
+	v, err := ajmath.UintToUint8(0)
+	assert.NoError(t, err)
+	assert.Equal(t, uint8(0), v)
+
+	v, err = ajmath.UintToUint8(42)
+	assert.NoError(t, err)
+	assert.Equal(t, uint8(42), v)
+
+	v, err = ajmath.UintToUint8(math.MaxUint8)
+	assert.NoError(t, err)
+	assert.Equal(t, uint8(math.MaxUint8), v)
+
+	v, err = ajmath.UintToUint8(math.MaxUint8 + 1)
+	assert.ErrorIs(t, err, ajmath.ErrIntegerOverflow)
+	assert.Equal(t, uint8(0), v)
+}
+
+func TestUintToUint16(t *testing.T) {
+	v, err := ajmath.UintToUint16(0)
+	assert.NoError(t, err)
+	assert.Equal(t, uint16(0), v)
+
+	v, err = ajmath.UintToUint16(42)
+	assert.NoError(t, err)
+	assert.Equal(t, uint16(42), v)
+
+	v, err = ajmath.UintToUint16(math.MaxUint16)
+	assert.NoError(t, err)
+	assert.Equal(t, uint16(math.MaxUint16), v)
+
+	v, err = ajmath.UintToUint16(math.MaxUint16 + 1)
+	assert.ErrorIs(t, err, ajmath.ErrIntegerOverflow)
+	assert.Equal(t, uint16(0), v)
+}
+
+func TestUintToUint32(t *testing.T) {
+	v, err := ajmath.UintToUint32(0)
+	assert.NoError(t, err)
+	assert.Equal(t, uint32(0), v)
+
+	v, err = ajmath.UintToUint32(42)
+	assert.NoError(t, err)
+	assert.Equal(t, uint32(42), v)
+
+	v, err = ajmath.UintToUint32(math.MaxUint32)
+	assert.NoError(t, err)
+	assert.Equal(t, uint32(math.MaxUint32), v)
+
+	v, err = ajmath.UintToUint32(math.MaxUint32 + 1)
+	assert.ErrorIs(t, err, ajmath.ErrIntegerOverflow)
+	assert.Equal(t, uint32(0), v)
+}
